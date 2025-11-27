aab129eed7bebe409d4bc04d3cbd86eb6f185aab
Roseira1212 <joao.roseira.ferrari@escola.pr.gov.br>
2025-11-26 23:02:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+const sons = {
+    'A' : 'boom.wav',
+    'S' : 'clap.wav',
+    'D' : 'hihat.wav',
+    'G' : 'kick.wav',
+    'H' : 'openhat.wav',
+    'I' : 'ride.wav',
+    'J' : 'snare.wav',
+    'K' : 'tink.wav',
+    'L' : 'tom.wav',
+};
+
+const criarDiv = (texto) => {
+    const div = document.createElement('div'); 
+    div.classList.add('key');
+    div.textContent = texto; 
+    div.id = texto;
+
+    // Evento de clique
+    div.addEventListener('click', () => {
+        tocarSom(texto);
+    });
+
+    document.getElementById('container').appendChild(div); 
+};
+
+Object.keys(sons).forEach(criarDiv);
+
+const adicionarEfeito = (letra) => {
+    const div = document.getElementById(letra);
+    if (!div) return;
+
+    div.classList.add('active');
+    setTimeout(() => div.classList.remove('active'), 150);
+};
+
+const tocarSom = (letra) => {
+    const som = sons[letra];
+    if (!som) return;
+    const audio = new Audio(`sons/${som}`);
+    audio.play();
+    adicionarEfeito(letra);
+};
+
+document.addEventListener('keydown', (event) => {
+    const letra = event.key.toUpperCase();
+    tocarSom(letra);
+});
